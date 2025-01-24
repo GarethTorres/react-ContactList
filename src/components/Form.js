40dca5b285b8c0e1e35fbbb8f299eecdf47f4029
Guizhi Xu <113871493+GarethTorres@users.noreply.github.com>
2025-01-24 12:58:10 -0500

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,66 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Form({ addContact }) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [website, setWebsite] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name === '' || email === '' || website === '') {
-      alert("Fields cannot be empty!");
-      // condition check
-
-    } else {
-      const newContact = {
-        // contact info update
-
-        id: Math.random(),  
-        // temporary ID
-        name,
-        email,
-        website
-
-
-      };
-      addContact(newContact);
-      setName('');
-      setEmail('');
-      setWebsite('');
-
-    }
-  };
-
+export default function Form() {
   return (
-    <form className="form" onSubmit={handleSubmit}>
+    <form className="form">
       <h1>Contact Form</h1>
       <input
         className="input"
-        placeholder="Name"
-
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        // changes update
-      />
+        placeholder="Name"/>
       <input
         className="input"
-        placeholder="Email"
-
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-
-      />
+        placeholder="Email"/>
       <input
         className="input"
-        placeholder="Website"
-
-        value={website}
-        onChange={(e) => setWebsite(e.target.value)}
-
-      />
-
+        placeholder="Website"/>
       <button className="btn" type="submit">Add</button>
-
-    </form>
-  );
+    </form> 
+  )
 }
